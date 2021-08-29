5d59e121c5f2f8e0231a534830f3308d44c98096
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-08-29 10:33:08 -0300

    Created functions to select and set order amounts

--- src/components/generics/Option.js
+++ src/components/generics/Option.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 export default function Option(props) {
     const {img, name, detail, price} = props.option;
+    const [selected, setSelected] = useState("");
+    const [amount, setAmount] = useState(1);
+
+    const select = () => {
+        if (selected === "") {
+            setSelected("selected");
+        }
+    }
+
+    const decreaseAmount = () => {
+        if (amount === 1) {
+            setSelected("");
+        } else {
+            setAmount(amount - 1);
+        }
+    }
+
+    const increaseAmount = () => {
+        setAmount(amount + 1);
+    }
+
     return (
-        <button class="option" onclick="selectDesert(this)">
+        <button className={`option ${selected}`} onClick={select}>
                 <img src={img} alt={name} />
-                <p class="name">{name}</p>
-                <p class="detail">{detail}</p>
-                <p class="price">R$ {price}</p>
-                <div class="amount">
-                    <span class="minus">-</span> 1 <span class="plus">+</span>
+                <p className="name">{name}</p>
+                <p className="detail">{detail}</p>
+                <p className="price">R$ {price}</p>
+                <div className="amount">
+                    <span className="minus" onClick={decreaseAmount}>-</span> {amount} <span className="plus" onClick={increaseAmount}>+</span>
                 </div>
         </button>
     );
