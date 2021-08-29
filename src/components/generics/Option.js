aba79f52b67143d24b5fd3d3f7e541bb2c774df3
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-08-29 10:06:01 -0300

    Added order amount on layout

--- src/components/generics/Option.js
+++ src/components/generics/Option.js
@@ -6,7 +6,9 @@ export default function Option(props) {
                 <p class="name">{name}</p>
                 <p class="detail">{detail}</p>
                 <p class="price">R$ {price}</p>
-                <ion-icon name="checkmark-circle"></ion-icon>
+                <div class="amount">
+                    <span class="minus">-</span> 1 <span class="plus">+</span>
+                </div>
         </button>
     );
 }
