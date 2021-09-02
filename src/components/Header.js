f10afc5179465da004f7af1f25e8879800c7d144
Bruno <mguimaraesbruno@gmail.com>
2021-09-02 12:03:22 -0300

    Code Review: Brunão

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="top-bar">
-            <p className="logo">DrivenEats</p>
-            <p className="slogan">Sua comida em 6 minutos</p>
-        </header>
-    );
-}
